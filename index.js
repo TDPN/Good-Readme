6b211f8c9747602edb47fe075fb2991ddb1ccbff
Don Nguyen <58411501+TDPN@users.noreply.github.com>
2020-03-06 00:36:21 -0800

    Uploaded new index.js

--- index.js
+++ index.js
@@ -1,12 +1,104 @@
-const questions = [
+const inquirer = require('inquirer');
+const util = require('util');
+const fs = require('fs');
+const api = require('./api')
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
+function promptUser() {
+ inquirer
+ .prompt([
+    {
+      type: 'input',
+      name: 'gitName',
+      message: 'What is your GitHub user name?'
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the Project Title?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project.'
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'Briefly explain and showcase what your application does.'
+    },
+    {
+      type: 'input',
+      name: 'licenses',
+      message: 'Please provide license information.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide details on your tests and how to run them.'
+    },
+    {
+      type: 'input',
+      name: 'acknowledgments',
+      message: 'Are there any acknowledgements you like to give?'
+    }
+
+  ]);
 }
 
-function init() {
+async function generateREADME(answers) {
+  const fullData = await api.getUser(answers);
 
+
+  return ` 
+      
+  # ${answers.title}
+  
+  ${answers.description}
+  
+  <!-- Animated Gif Here -->
+  
+  ## Usage
+  
+  ${answers.instructions}
+  
+  
+  ## Running the tests
+  
+  ${answers.tests}
+  
+  ## Author
+  
+  #${fullData.data.name} 
+  <img src="${fullData.data.avatar_url}" alt="Author Picture" width="150">
+  Git: ${fullData.data.login} | email: ${fullData.data.email}
+  
+  ## License
+  
+  ${answers.licenses}
+  
+  ## Acknowledgments
+  
+  ${answers.acknowledgements}
+  
+  ## Badges
+      
+      `;
 }
 
+async function init() {
+  try {
+    const answers = await promptUser();
+
+    const text = await generateREADME(answers);
+
+    await writeFileAsync("README.md", text);
+
+    console.log("Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
+// Initiates process.
 init();
